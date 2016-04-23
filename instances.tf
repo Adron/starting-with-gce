2b2095ccd58760275ef8efbd4a1f630647a22f0e
Adron Hall <adronhall@gmail.com>
2016-04-22 18:17:38 -0700

    First commit.

---
+++ instances.tf
@@ -0,0 +1,20 @@
+# Create a new instance
+resource "google_compute_instance" "flirpderp" {
+    name = "flirpderp"
+    machine_type = "f1-micro"
+
+    zone = "us-central1-b"
+
+    disk {
+        image = "ubuntu-1404-trusty-v20160406"
+    }
+
+    network_interface {
+        network = "default"
+        access_config {}
+    }
+
+    service_account {
+        scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+    }
+}
